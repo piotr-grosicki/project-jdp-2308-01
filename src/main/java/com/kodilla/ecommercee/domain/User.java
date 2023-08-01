1436f91746db9716b3a320f6c31fe0e20d483ebb
DanielKryszkiewicz <kryszkiewicz.daniel1@gmail.com>
2023-08-01 22:29:07 +0200

    JDP230801-15

---
+++ src/main/java/com/kodilla/ecommercee/domain/User.java
@@ -0,0 +1,31 @@
+package com.kodilla.ecommercee.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String email;
+
+
+
+  //  @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+  //  private List<Order> orders;
+
