49715f4323dd10b116f9d9c6903d82b20c5e58ef
Kasia Gierasimczuk <k.gierasimczuk@gmail.com>
2023-08-07 16:35:01 +0200

    Cart entity and repository creation

--- src/main/java/com/kodilla/ecommercee/domain/Cart.java
+++ src/main/java/com/kodilla/ecommercee/domain/Cart.java
@@ -3,21 +3,57 @@ package com.kodilla.ecommercee.domain;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+
+import javax.persistence.*;
+import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
+@Setter
 @Getter
 @Entity(name = "CARTS")
 public class Cart {
 
+
     @Id
     @GeneratedValue
-    private long id;
-    private long customerId;
+    private Long id;
 
+
+    //private Customer customer;
+    //private Product product;
+    //private Order order;
+    //private List<Product> items;
+
+
+    /*
+    @OneToOne(cascade = CascadeType.ALL, mappedBy = "customers")
+    @JoinColumn(name = "customers_id")
+    public Customer getCustomer(){
+        return customer;
+    }
+     */
+
+    /*
+    @ManyToMany(cascade = CascadeType.ALL, mappedBy = "carts")
+    @JoinTable(
+        name = "products",
+        joinColumns = {@JoinColumn(name = "products_product_id", referencedColumnName = "products_product_id")},
+        inverseJoinColumns = {@JoinColumn(name = "carts_id", referencedColumnName = "carts_id")}
+    public List<Product> getProducts() {
+        return items;
+    }
+    */
+
+
+    /*
+    @OneToOne(cascade = CascadeType.ALL, mappedBy = "orders")
+    @JoinColumn(name = "orders_id")
+    public Order getOrder(){
+        return order;
+    }
+    */
 
 }
