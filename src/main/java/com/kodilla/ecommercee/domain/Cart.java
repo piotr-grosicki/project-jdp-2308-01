f5d6f340a359465d73d8378db4037ca5288cf6af
Barhwald <bylinabass@gmail.com>
2023-08-12 23:20:20 +0200

    Cart entity refactored

--- src/main/java/com/kodilla/ecommercee/domain/Cart.java
+++ src/main/java/com/kodilla/ecommercee/domain/Cart.java
@@ -16,43 +16,23 @@ import java.util.List;
 @Entity(name = "CARTS")
 public class Cart {
 
-
     @Id
     @GeneratedValue
     private Long id;
 
-
-    //private Customer customer;
-    //private Order order;
-    //private List<Product> items;
-
-
-    /*
     @OneToOne(cascade = CascadeType.ALL, mappedBy = "customers")
     @JoinColumn(name = "customers_id")
-    public Customer getCustomer(){
-        return customer;
-    }
-     */
+    private Customer customer;
 
-    /*
+
+//    @OneToOne(cascade = CascadeType.ALL, mappedBy = "orders")
+//    @JoinColumn(name = "orders_id")
+//    private Order order;
+
     @ManyToMany(cascade = CascadeType.ALL, mappedBy = "carts")
     @JoinTable(
-        name = "products",
-        joinColumns = {@JoinColumn(name = "products_product_id", referencedColumnName = "products_product_id")},
-        inverseJoinColumns = {@JoinColumn(name = "carts_id", referencedColumnName = "carts_id")}
-    public List<Product> getProducts() {
-        return items;
-    }
-    */
-
-
-    /*
-    @OneToOne(cascade = CascadeType.ALL, mappedBy = "orders")
-    @JoinColumn(name = "orders_id")
-    public Order getOrder(){
-        return order;
-    }
-    */
-
+            name = "products",
+            joinColumns = {@JoinColumn(name = "products_product_id", referencedColumnName = "products_product_id")},
+            inverseJoinColumns = {@JoinColumn(name = "carts_id", referencedColumnName = "carts_id")})
+    private List<Product> items;
 }
