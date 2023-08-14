410cd2863d7ea0e76583ac01cdf619f3a83d98a0
Barhwald <bylinabass@gmail.com>
2023-08-15 00:54:35 +0200

    OrderRepoTest created

--- src/main/java/com/kodilla/ecommercee/domain/Customer.java
+++ src/main/java/com/kodilla/ecommercee/domain/Customer.java
@@ -6,6 +6,9 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
 @Getter
 @Setter
 @AllArgsConstructor
@@ -29,8 +32,8 @@ public class Customer {
     @Column(nullable = false)
     private String customerKey;
 
-//  @OneToMany(mappedBy = "customer", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-//  private List<Order> orders;
+    @OneToMany(mappedBy = "customer", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Order> orders = new ArrayList<>();
 
 
     public Customer(String login, String email, boolean isLocked, String customerKey) {
@@ -38,6 +41,14 @@ public class Customer {
         this.email = email;
         this.isLocked = isLocked;
         this.customerKey = customerKey;
+    }
+
+    public Customer(String login, String email, boolean isLocked, String customerKey, List<Order> orders) {
+        this.login = login;
+        this.email = email;
+        this.isLocked = isLocked;
+        this.customerKey = customerKey;
+        this.orders = orders;
     }
 }
 
--- src/test/java/com/kodilla/ecommercee/domain/OrderRepositoryTest.java
+++ src/test/java/com/kodilla/ecommercee/domain/OrderRepositoryTest.java
@@ -1,6 +1,8 @@
 package com.kodilla.ecommercee.domain;
 
 
+import com.kodilla.ecommercee.repository.CartRepository;
+import com.kodilla.ecommercee.repository.CustomerRepository;
 import com.kodilla.ecommercee.repository.OrderRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,8 +13,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
@@ -20,6 +21,12 @@ public class OrderRepositoryTest {
 
     @Autowired
     private OrderRepository orderRepository;
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Autowired
+    private CartRepository cartRepository;
 
     @Test
     void getOrderById() {
@@ -61,6 +68,92 @@ public class OrderRepositoryTest {
         assertEquals(2, orderList.size());
         assertTrue(orderRepository.findById(id1).isPresent());
         assertTrue(orderRepository.findById(id2).isPresent());
+    }
+
+    @Test
+    void updateOrder() {
+        //Given
+        Cart cart = new Cart();
+        Customer customer1 = new Customer("JavaMan", "jm@gmail.com", false, "efrsdvb546ujtngf");
+        Customer customer2 = new Customer("ManMan", "awfweg@gmail.com", false, "hnkuunyerbtesr");
+        Order order = new Order(cart, customer1);
+
+        //When
+        orderRepository.save(order);
+        order.setCustomer(customer2);
+        orderRepository.save(order);
+        long id1 = order.getId();
+
+        //Then
+        assertTrue(orderRepository.findById(id1).isPresent());
+        assertEquals("ManMan", orderRepository.findById(id1).get().getCustomer().getLogin());
+
+    }
+
+    @Test
+    void deleteOrder() {
+        //Given
+        Cart cart = new Cart();
+        Customer customer = new Customer("JavaMan", "jm@gmail.com", false, "efrsdvb546ujtngf");
+        Order order = new Order(cart, customer);
+
+        //When
+        orderRepository.save(order);
+        long id = order.getId();
+        orderRepository.deleteById(id);
+
+        //Then
+        assertFalse(orderRepository.existsById(id));
+    }
+
+    @Test
+    void oneCustomerManyOrders() {
+        //Given
+        Cart cart = new Cart();
+        List<Order> orders = new ArrayList<>();
+        Customer customer1 = new Customer("JavaMan", "jm@gmail.com", false, "efrsdvb546ujtngf", orders);
+        Order order1 = new Order(cart, customer1);
+        Order order2 = new Order(cart, customer1);
+        orders.add(order1);
+        orders.add(order2);
+
+        //When
+        customerRepository.save(customer1);
+        orderRepository.save(order1);
+        orderRepository.save(order2);
+        long customerId = customer1.getCustomerId();
+
+        //Then
+        assertTrue(customerRepository.findById(customerId).isPresent());
+        assertEquals(2, customerRepository.findById(customerId).get().getOrders().size());
+    }
+
+    @Test
+    void oneCartOneOrder() {
+        //Given
+        Customer customer1 = new Customer("JavaMan", "jm@gmail.com", false, "efrsdvb546ujtngf");
+        Cart cart1 = new Cart();
+        Order order1 = new Order(cart1, customer1);
+        Cart cart2 = new Cart();
+        Customer customer2 = new Customer("ManMan", "awfweg@gmail.com", false, "hnkuunyerbtesr");
+        Order order2 = new Order(cart2, customer2);
+        cart1.setOrder(order1);
+        cart2.setOrder(order2);
+
+        //When
+        orderRepository.save(order1);
+        orderRepository.save(order2);
+        long order1Id = order1.getId();
+        long order2Id = order2.getId();
+        long cart1Id = cart1.getId();
+        long cart2Id = cart2.getId();
+
+        //Then
+        assertTrue(cartRepository.findById(cart1Id).isPresent());
+        assertTrue(cartRepository.findById(cart2Id).isPresent());
+        assertEquals(order1Id, cartRepository.findById(cart1Id).get().getOrder().getId().longValue());
+        assertEquals(order2Id, cartRepository.findById(cart2Id).get().getOrder().getId().longValue());
+
     }
 
 }
