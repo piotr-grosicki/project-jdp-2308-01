7d627d3a4472577d345762c6118b349b1df49aeb
Barhwald <bylinabass@gmail.com>
2023-08-14 22:50:27 +0200

    ProductMapper updated

--- src/main/java/com/kodilla/ecommercee/mapper/ProductMapper.java
+++ src/main/java/com/kodilla/ecommercee/mapper/ProductMapper.java
@@ -3,7 +3,6 @@ package com.kodilla.ecommercee.mapper;
 import com.kodilla.ecommercee.domain.Product;
 import com.kodilla.ecommercee.domain.ProductDto;
 import com.kodilla.ecommercee.repository.GroupRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
