4754ec64c960cd163e0deae07eae41fa129650ad
Barhwald <bylinabass@gmail.com>
2023-08-15 19:25:39 +0200

    CrossOrigin deleted

--- src/main/java/com/kodilla/ecommercee/controller/GroupController.java
+++ src/main/java/com/kodilla/ecommercee/controller/GroupController.java
@@ -14,7 +14,6 @@ import java.util.List;
 @RestController
 @RequestMapping("/v1/groups")
 @RequiredArgsConstructor
-@CrossOrigin("*")
 public class GroupController {
 
     private final DbService dbService;
